1a62e8eb1ed7bea360e47610e045357853b626cb
steffi <stephanielindner@outlook.de>
2023-01-05 16:23:51 +0100

    now checkup

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const searchHeight = document.querySelector(".search").clientHeight;
 const mainBackground = document.querySelector(".crop_background");
-const windowHeight = window.innerHeight;
 
+const windowHeight = window.innerHeight;
 
 // Set background waves of main component according to percentage of screensize
 const offsetBackground = ((100/windowHeight)*searchHeight);
